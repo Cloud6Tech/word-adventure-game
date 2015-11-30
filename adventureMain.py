ef32b57b6df0448bb78a72eac364c9651058143d
hmccabe <hmccabe@csumb.edu>
2015-11-30 12:14:55 -0800

    Fixed inspect for bear, painting
    - Killing bear or inspecting painting now changes associated room
    description
    
    - Inspecting table now updates bear description to killing text

--- adventureMain.py
+++ adventureMain.py
@@ -44,11 +44,12 @@ def mainFunc():
                     "end you were praying for. You have fallen to your death.")
   
   # Knife Room
-  roomKnife = room("Torture Chamber", "You immediately notice the warmth of a fire upon your skin as you walk through the door's threshold. " + \
+  roomKnifeBaseDescription = "You immediately notice the warmth of a fire upon your skin as you walk through the door's threshold. " + \
                    "You begin to feel yourself naturally relax as a result of the fire. This quickly dissipates as you take a look around " + \
                    "to see the room is decorated with devices that could serve no other purpose than torture. Directly across from you to " + \
                    "the West, next to the fire, is a single table lined with assorted tools. Looking to North you notice yet another " + \
-                   "door, meaning a continuation to the nightmare. To the South nothing immediately catches your eye, however, after a " + \
+                   "door, meaning a continuation to the nightmare."
+  roomKnife = room("Torture Chamber", roomKnifeBaseDescription + " To the South nothing immediately catches your eye; however, after a " + \
                    "second look you can't help but notice an elaborate painting of a lovely woman dancing under a pale moon's light.")
                     
   # Developer Credits Room
@@ -57,10 +58,11 @@ def mainFunc():
                      "part of a sinister game. All is not lost... if this is a game then there must be a way to win!") 
   
   # Bear Room
-  roomBear = room("Guardian's Quarters","Natural light seems for fill this room and you hope this is your exit. You follow the light to find the source is " + \
-                  "an opening high in the ceiling, but you see no way to climb up to it. Directly across from you to the East, you see a door which " + \
-                  "appears to be blocked by a monstrous bear. To your relief the animal is asleep, you will need some sort of weapon to get " + \
-                  "past. To the North you spot a small opening which may contain an answer. How do you wish to proceed?")  
+  roomBearBaseDescription = "Natural light seems for fill this room and you hope this is your exit. You follow the light to find the source is " + \
+                            "an opening high in the ceiling, but you see no way to climb up to it."
+  roomBear = room("Guardian's Quarters",roomBearBaseDescription + " Directly across from you to the East, you see a door which " + \
+                  "appears to be blocked by a monstrous bear. To your relief the animal is asleep, but you will need some sort of weapon to get " + \
+                  "past. To the North you spot a small opening which may contain an answer.")  
   
   # Riddle Room
   roomRiddle = room("Prisoner's Cell", "You enter what appears to be a forgotten room. Old spider webs fill the dark crevices and thick carpet of dust " + \
@@ -81,16 +83,28 @@ def mainFunc():
                      " You feel the warmth of the afternoon sun of you face and realize you have made it out alive.")
     
   #--- --- --- --- --- --- Add Inspectable Items --- --- --- --- --- --- ---
-  # Knife on table in Torture Room; inspecting gives the player a knife
+  # Bear in Guardian's Quarters; inspecting will kill the bear if the player has a knife, creating an East door
+  def bearDoor():
+    if prot.searchInventory(1) == true :
+      roomBear.setExit("EAST", roomRiddle)
+      roomBear.setDescription(roomBearBaseDescription + " To the East, there is the carcass of a bear near a door. " + \
+                              "To the North you spot a small opening.")
+      roomBear.setInspect("BEAR","The bear is quite dead.")
+  roomBear.setInspect("BEAR","A bear sleeps in front of a door and you cannot go through without disturbing it. You need something to kill it.", bearDoor)
+    
+  # Knife on table in Torture Room; inspecting gives the player a knife and changes the bear interaction
   def tableKnife():
     if not prot.searchInventory(itemKnife.getId()):
       prot.addToInventory(itemKnife)
       roomKnife.removeInspect("TABLE")
+      roomBear.setInspect("BEAR","You remember the knife in your pocket and quickly use it to kill the sleeping bear. The way east is now clear.",bearDoor)
   roomKnife.setInspect("TABLE","At the end of the table you spot a long jagged dagger which could be useful in a dangerous situation. You take it.",tableKnife)
   
   # Painting on wall of Torture Chamber; inspecting creates a South door
   def paintingDoor():
-    roomKnife.setExit("S", roomSecret)
+    roomKnife.setExit("SOUTH", roomSecret)
+    roomKnife.setDescription(roomKnifeBaseDescription + " To the South, behind an elaborate painting of a lovely woman dancing under a pale moon's light, " + \
+                             "there is another door.")
   roomKnife.setInspect("PAINTING","Upon closer examination you see that the painting is wavering ever so slightly. You step a bit closer " + \
                       " and feel a cool breeze and the smell of fresh air. You've found a door hidden behind the painting.",paintingDoor)
                       
@@ -100,22 +114,16 @@ def mainFunc():
                          "Jason Lloyd - Slayer of Pythons and master of all things logistical\n" + \
                          "Brett Hansen - He's pretty cool too I guess")                    
                       
-  # Bear in Guardian's Quarters; inspecting will kill the bear if the player has a knife
-  def bearDoor():
-    if prot.searchInventory(1) == true :
-      roomBear.setExit("E", roomRiddle)
-      printNow ("You have killed the sleeping bear with the knife. The way east is now clear.")
-      return "You have killed the sleeping bear with the knife. The way east is now clear."
-  roomBear.setInspect("BEAR","A bear sleeps in front of a door and you cannot go through without disturbing it. You need something to kill it.", bearDoor)
-  
   # Plaque in Prisoner's Cell; inspecting prompts the player to solve a riddle
   def riddle():
     riddleGuess = requestString("Speak the answer to the riddle: ")
-    if riddleGuess.upper() == "LIFE":
+    if riddleGuess == None:
+      return
+    elif riddleGuess.upper() == "LIFE":
       printNow( "Here's the passwordddddddddddddddd. password1234")
       return "Here's the passwordddddddddddddddd. password1234"
     else:
-      printNow ("That is incorrect. Comeback when you are ready to try again")
+      printNow ("That is incorrect. Come back when you are ready to try again.")
       return "Nothing happens."
   roomRiddle.setInspect("PLAQUE", "The plaque reads: 'It has a white light at the end of it and can be over in the blink of an eye. If you have seen " + \
                         "our secret, you know its value. What do I speak of?'",riddle)     
